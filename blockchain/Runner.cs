7af2dbc82cb9b6867290f5e37423f685c1f91af7
David Cumps <david@cumps.be>
2021-12-15 13:49:38 +0100

    wip: poc send WS message

--- blockchain/Runner.cs
+++ blockchain/Runner.cs
@@ -1,11 +1,14 @@
 ﻿namespace Blockchain
 {
     using System;
+    using System.Net.WebSockets;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Configuration;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
+    using Newtonsoft.Json;
 
     public class Runner
     {
@@ -22,13 +25,56 @@
         }
 
         public async Task StartAsync(
-            CancellationToken cancellationToken)
+            CancellationToken ct)
         {
             _logger.LogInformation("Starting {TaskName}.", nameof(Runner));
 
-            await Task.Delay(10000, cancellationToken);
+            await Bla(ct);
 
             // TODO: Implement
+        }
+
+        private async Task Bla(
+            CancellationToken ct)
+        {
+            var ws = new ClientWebSocket();
+            await ws.ConnectAsync(new Uri("ws://127.0.0.1:8080"), ct);
+
+            await SendAsync(
+                new
+                {
+                    type = "identity",
+                    payload = new
+                    {
+                        identity = "bwBpkJX5VAtFOt2K/DFuJ+KrlkWy2YNDkxe19TKnlCI=",
+                        version = 11,
+                        agent = "if/cli/4b3079f",
+                        name = "csharp-node",
+                        port = 9033,
+                        head = "8fb5b576eb519e03911c8768d7c8ac97252f7be49fec77279c43af2da9f82068",
+                        work = "131072",
+                        sequence = 1
+                    }
+                },
+                ws,
+                ct);
+
+            Console.WriteLine(ws.State);
+        }
+
+        private static async Task SendAsync<T>(
+            T message,
+            WebSocket ws,
+            CancellationToken ct)
+        {
+            var json = JsonConvert.SerializeObject(message);
+            var bytes = Encoding.UTF8.GetBytes(json);
+
+            await ws.SendAsync(
+                new ArraySegment<byte>(bytes),
+                WebSocketMessageType.Text,
+                WebSocketMessageFlags.EndOfMessage,
+                ct);
         }
     }
 }
